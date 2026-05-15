b1d30847c8b6341e738059ff670f4530169edb5e
caeia <140228298+caesij@users.noreply.github.com>
2026-05-15 11:36:07 +0800

    DB setup

--- cafeproject/cafeproject/settings.py
+++ cafeproject/cafeproject/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sv%irk0$-0s=u*(0$)lbx+bb2-%yeu*d3qt$xj7*po+6nw8#ca'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
@@ -74,12 +78,14 @@ WSGI_APPLICATION = 'cafeproject.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default = os.getenv("DATABASE_URL"),
+        conn_max_age = 600,
+        conn_health_checks = True,
+    )
 }
 
+DATABASES['default']['OPTIONS'] = {'sslmode': 'require' if not os.getenv("DEBUG") == "True" else 'prefer'}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
